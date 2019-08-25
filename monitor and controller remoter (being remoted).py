07fddcd799b8eba8bbb42a4ad27b7d957da182d4
timberh <54344965+timberh@users.noreply.github.com>
2019-08-25 18:46:11 +0800

    Update monitor and controller remoter (being remoted).py

--- monitor and controller remoter (being remoted).py
+++ monitor and controller remoter (being remoted).py
@@ -2,13 +2,17 @@ from pynput.mouse import Button, Controller
 from pynput import mouse, keyboard
 from pynput.keyboard import Key 
 import threading
+from socket import*
+from PIL import ImageGrab
 
 mouse = Controller()
 
 listener = mouse.Listener(   #monitoring the mouse
     on_move=onmove,
     on_click=onclick,
-    on_scroll=onscroll)
+    on_scroll=onscroll,
+    on_press=onpress.
+    on_release=onrelease)
 listener.start()
 
 def setmouse (x,y):#controlling mouse
@@ -83,8 +87,60 @@ def key3 (key1,key2,key3):
 #monitor keyboard
 
 def onpress(key):
+    sendmsg(key)
 
 def onrelease(key):
+    sendmsg(key)
 
 
 #keyboard end
+
+def sendmsgcall(host='0.0.0.0', port= '5000'):
+    monitor = socket()
+    monitor.connect(host,port)
+
+def screenshot()
+    try:
+        monitor.listen(5)
+        golbal end(0)
+        conn,addr = monitor.accept()
+        thread = Thread(target=retreive_screenshot, args=(conn,))
+        thread.start()
+#screenshot
+
+             
+def retreive_screenshot(conn):
+    with mss() as sct:
+        # The region to capture
+        rect = {'top': 0, 'left': 0, 'width': WIDTH, 'height': HEIGHT}
+
+        while 'recording':
+            # Capture the screen
+            img = sct.grab(rect)
+            # Tweak the compression level here (0-9)
+            pixels = compress(img.rgb, 6)
+
+            # Send the size of the pixels length
+            size = len(pixels)
+            size_len = (size.bit_length() + 7) // 8
+            conn.send(bytes([size_len]))
+
+            # Send the actual pixels length
+            size_bytes = size.to_bytes(size_len, 'big')
+            conn.send(size_bytes)
+
+            # Send pixels
+            conn.sendall(pixels)
+
+#recev
+
+def recv():
+    data = monitor.recvfrom()
+
+def sendmsg(msg):
+    monitor.sendall(msg)
+
+
+
+
+             
